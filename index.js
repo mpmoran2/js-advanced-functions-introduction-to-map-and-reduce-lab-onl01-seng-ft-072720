39b9c28af5ad8d47e8965528c3c7d619b0bc03ef
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-10 18:00:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Your code here
+
+//map-like
+function mapToNegativize(array){
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i] * -1)
+    }
+    return newArray
+}
+
+function mapToNoChange(array){
+    let newArray = []
+    for (let i = 0; i < array.length; i++){
+        newArray.push(array[i])
+    }
+    return newArray
+}
+
+function mapToDouble(array){
+    let newArray = []
+    for (let i = 0; i < array.length; i++){
+        newArray.push(array[i] * 2)
+    }
+    return newArray
+}
+
+function mapToSquare(array){
+    let newArray = []
+    for (let i = 0; i < array.length; i++){
+        newArray.push(array[i] * array[i])
+    }
+    return newArray
+}
+
+//reduce-like
+function reduceToTotal(array, start = 0){
+    for (let i = 0; i < array.length; i++){
+        start += array[i]
+    }
+    return start
+}
+
+function reduceToAllTrue(array){
+    let answer = true; 
+    for (let i = 0; i < array.length; i++){
+        if (array[i] === false) {
+            answer = false
+        }
+    }
+    return answer
+}
+
+function reduceToAnyTrue(array) {
+    let answer = false;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === true) {
+            answer = true
+        }
+    }
+    return answer
+}
